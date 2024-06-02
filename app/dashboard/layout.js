c0b3267837d90f06a4cb5b0b75fa25a074ecd558
Manish Gudewar <202152322@iiitvadodara.ac.in>
2024-06-02 21:20:09 +0530

    name changes

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,9 +1,8 @@
 import Navbar from "@/Components/navbar";
-import Image from "next/image";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col md:flex-row min-h-screen w-full">
+    <div id="__next" className="flex flex-col md:flex-row min-h-screen w-full">
       <div>
         <Navbar />
       </div>
--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
         <div>{children}</div>
       </body>
